dd64cfe4f9b763311bf82f8becb3f83889917564
root <root@imrny2c.mylabserver.com>
2019-10-18 23:42:20 +0000

    Refactored install class for Debian family servers

---
+++ manifests/params.pp
@@ -0,0 +1,17 @@
+# @summary
+# OS related variables and others
+# A description of what this class does
+#
+# @example
+#   include apache::params
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
